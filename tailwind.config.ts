6175bfc56e97bd4bb53277218103cfda71a35f38
Alex Clemens <alexsydneynsw@gmail.com>
2024-04-30 12:12:06 +1000

    calculate secondary color automatically

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,15 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+const hexToRGBA = (hex: string, opacity: number): string => {
+  let r = parseInt(hex.slice(1, 3), 16);
+  let g = parseInt(hex.slice(3, 5), 16);
+  let b = parseInt(hex.slice(5, 7), 16);
+  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+}
+
+const mainColorHex = "#147D73"
+
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
@@ -8,8 +17,8 @@ export default {
       colors: {
         "foboh-field": "#F8FAFC",
         "foboh-panel": "#FFFFFF",
-        "foboh-main": "#147D73",
-        "foboh-main-disabled": "#9CB6B4",
+        "foboh-main": mainColorHex,
+        "foboh-main-disabled": hexToRGBA(mainColorHex, 0.9),
         "foboh-impact": "#DC3545",
         "foboh-text": "#637381",
         "foboh-secondary": "#d9d9d9",
